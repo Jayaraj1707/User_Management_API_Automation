e26af4bf0da515e0db20114b90f83860009b650e
Jayaraj1707 <96728893+Jayaraj1707@users.noreply.github.com>
2024-11-04 11:48:08 +0530

    Update PrintSout.java

--- PrintSout.java
+++ PrintSout.java
@@ -4,6 +4,5 @@ public class PrintSout {
     public static void main(String[] args) {
       System.out.println("Good Morning, Have a good day!.");
       System.out.println("Happy WeekEnd!.");
-        System.out.println("Hey Ram!.");
     }
 }
