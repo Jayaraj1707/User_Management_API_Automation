b1ed95e64f1ad1019ae0e2fed900f49f89f881ac
Jayaraj1707 <96728893+Jayaraj1707@users.noreply.github.com>
2024-11-04 10:24:14 +0530

    Add files via upload

---
+++ PrintSout.java
@@ -0,0 +1,8 @@
+
+public class PrintSout {
+
+    public static void main(String[] args) {
+      System.out.println("Good Morning, Have a good day!.");
+      System.out.println("Happy WeekEnd!.");
+    }
+}
